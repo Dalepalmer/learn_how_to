f68b0330f45e13a1e333da48d6e202bd6bfc65ec
Dale <dap38@zips.uakron.edu>
2015-03-17 15:26:09 -0700

    Added chapters and Lessons

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'uglifier'
 gem 'coffee-rails'
 gem 'jquery-rails'
 gem 'turbolinks'
+gem 'launchy'
 
 group :development do
   gem 'byebug'
@@ -17,6 +18,7 @@ end
 
 group :test, :development do
   gem 'rspec-rails'
+  gem 'capybara'
 end
 
 group :test do
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150317162732) do
+ActiveRecord::Schema.define(version: 20150317212355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "lessons", force: :cascade do |t|
+  create_table "chapters", force: :cascade do |t|
     t.string "name"
-    t.string "content"
+  end
+
+  create_table "lessons", force: :cascade do |t|
+    t.string  "name"
+    t.string  "content"
+    t.integer "number"
+    t.integer "chapter_id"
   end
 
 end
